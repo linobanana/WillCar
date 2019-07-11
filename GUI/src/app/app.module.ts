dd0ca23d351a3b4fceab450f01425d990ca94986
linobanana <linobanana@gmail.com>
2019-07-11 15:35:07 +0300

    header created

--- GUI/src/app/app.module.ts
+++ GUI/src/app/app.module.ts
@@ -6,20 +6,34 @@ import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
 import {routes} from "./app.routes";
-import {MainModule} from "./pages/main/main.module";
+import { MainModule } from "./pages/main/main.module";
 import { HeaderComponent } from './shared/components/header/header.component';
+import { MaterialModule } from './shared/components/material.module';
+import { MenuComponent } from './shared/components/header/menu/menu.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule,
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatNativeDateModule,
     HttpClientModule,
     RouterModule.forRoot(routes, {useHash: true}),
-    MainModule
+    MainModule,
+    MaterialModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- GUI/src/app/shared/components/header/header.component.ts
+++ GUI/src/app/shared/components/header/header.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {NgModule} from '@angular/core';
+import { Component } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {MatNativeDateModule} from '@angular/material';
+import {MaterialModule} from '../material.module';
+import {MenuComponent} from './menu/menu.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+  entryComponents: [MenuComponent],
+  declarations: [MenuComponent],
+  bootstrap: [MenuComponent],
+  providers: []
+})
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class HeaderComponent {
 
 }
