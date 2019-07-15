9d56ba565d9a5c2526e4d5958252eccd6207ab5d
EgorSemenov <wildworld2727@tut.by>
2019-07-15 11:38:29 +0300

    removed LM path

--- GUI/src/app/app.routes.ts
+++ GUI/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from '@angular/router';
-import {LeftMenuComponent} from './pages/main/left-menu/left-menu.component';
 import {MainRoutes} from './pages/main/main.routes';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'main', pathMatch: 'full'},
-  ...MainRoutes,
-  { path: 'LM', component: LeftMenuComponent }
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  },
+  ...MainRoutes
 ];
