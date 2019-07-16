e98b178b807f3866772a5101603be533718ddb20
yuliaminkevich <y3602061@gmail.com>
2019-07-16 13:44:35 +0300

    CarRepository

--- BackEnd/src/main/java/com/exadel/carpoolfree/repository/CarRepository.java
+++ BackEnd/src/main/java/com/exadel/carpoolfree/repository/CarRepository.java
@@ -3,5 +3,8 @@ package com.exadel.carpoolfree.repository;
 import com.exadel.carpoolfree.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CarRepository extends JpaRepository<Car, Long> {
+    List<Car> findAllByUserId(Long userId);
 }
