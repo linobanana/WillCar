7b331aeddca6465fa24e951f689e2b1950ea7ff9
yuliaminkevich <y3602061@gmail.com>
2019-07-22 17:49:07 +0300

    delete baseRepository

--- BackEnd/src/main/java/com/exadel/carpoolfree/repository/BaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exadel.carpoolfree.repository;
-
-import com.exadel.carpoolfree.model.BaseEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, Long> {
-}
