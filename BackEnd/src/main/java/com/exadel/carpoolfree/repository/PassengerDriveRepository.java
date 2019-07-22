54e413697563b62a1c2410e8059eb4e110a1f538
yuliaminkevich <y3602061@gmail.com>
2019-07-22 17:46:10 +0300

    drives for passenger

--- BackEnd/src/main/java/com/exadel/carpoolfree/repository/PassengerDriveRepository.java
+++ BackEnd/src/main/java/com/exadel/carpoolfree/repository/PassengerDriveRepository.java
@@ -2,6 +2,16 @@ package com.exadel.carpoolfree.repository;
 
 import com.exadel.carpoolfree.model.PassengerDrive;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PassengerDriveRepository extends JpaRepository<PassengerDrive, Long> {
+
+
+    @Query(value = "SELECT d " +
+            "from PassengerDrive d " +
+            "left join fetch d.passenger p  " +
+            "where p.id = :passengerId")
+    List<PassengerDrive> findAllByPassengerId(Long passengerId);
 }
