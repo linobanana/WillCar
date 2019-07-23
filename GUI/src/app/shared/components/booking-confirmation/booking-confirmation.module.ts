b41e04198018f041e1db0927c25a15a2abe74c5d
linobanana <linobanana@gmail.com>
2019-07-23 12:35:53 +0300

    added proxy, role switching, fixed some styles

--- GUI/src/app/shared/components/booking-confirmation/booking-confirmation.module.ts
+++ GUI/src/app/shared/components/booking-confirmation/booking-confirmation.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {BookingConfirmationComponent} from './booking-confirmation.component';
+import {MatButtonModule} from '@angular/material';
 
 
 @NgModule({
   imports: [
+    MatButtonModule
 
   ],
   declarations: [
