f82e970a78fb4be5adb7a17fb0dbd7dc86911c5e
OrexAn <fenixuzumake@gmail.com>
2019-07-24 12:18:51 +0300

    added pom.xml

--- BackEnd/src/main/java/com/exadel/carpoolfree/CarpoolfreeApplication.java
+++ BackEnd/src/main/java/com/exadel/carpoolfree/CarpoolfreeApplication.java
@@ -2,12 +2,19 @@ package com.exadel.carpoolfree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
 public class CarpoolfreeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CarpoolfreeApplication.class, args);
+    }
+
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
     }
 
 }
